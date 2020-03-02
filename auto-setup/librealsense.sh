d3b0d2ee1237b9a24ecc43d6ca238535da846d7b
Fal318 <uznaytiuy0625@gmail.com>
2020-03-02 16:50:09 +0900

    Update setup script for librealsense.

--- auto-setup/librealsense.sh
+++ auto-setup/librealsense.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
+sudo apt update
+sudo apt -y full-upgrade
+sudo apt -y install python3-dev python3-pip git 
 
-yes | sudo apt update
-yes | sudo apt -y full-updrade
-yes | sudo apt -y install python3-dev python3-pip git 
-yes | sudo apt -y install libssl-dev libusb-1.0-0-dev pkg-config libgtk-3-dev
-yes | pip3 install numpy opencv-python  matplotlib serial
-yes | cd ~
-yes | git clone https://github.com/IntelRealSense/librealsense.git
-yes | cd librealsense
-yes | sudo ./scripts/patch-realsense-ubuntu-xenial-joule.sh
-yes | mkdir build && cd build
-yes | cmake ../ -DCMAKE_BUILD_TYPE=Release -DBUILD_EXAMPLES=true
-yes | sudo make install
-yes | pip3 install pyrealsense2
-yes | rm -rf ~/librealsense
+pip3 install numpy opencv-python  matplotlib serial
+
+sudo apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
+
+sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo bionic main" -u
+
+sudo apt -y install librealsense2-dkms librealsense2-utils librealsense2-dev librealsense2-dbg
+pip3 install pyrealsense2
