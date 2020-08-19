fae17dd1f055dcde833a1539b5a206c778e29531
Fal318 <uznaytiuy0625@gmail.com>
2020-08-19 16:40:02 +0900

    :bug:ログが出力されない問題を修正

--- env-setup/pip/update_pip.py
+++ env-setup/pip/update_pip.py
@@ -1,18 +1,36 @@
 """This program is upgrade pip packages"""
+import sys
 import subprocess
 
-update_package = [
-    o.split(" ")[0] for o in subprocess.getoutput("python3 -m pip list -o").split("\n")[2:]
-]
+def get_lines(cmd):
+    """output logs"""
+    proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
-for packages in update_package:
-    subprocess.getoutput(f"python3 -m pip install -U {packages}")
+    while True:
+        line = proc.stdout.readline()
+        if line:
+            print(line.decode('utf-8'))
 
-while True:
-    broken_package = [
-        o.split(" ")[0] for o in subprocess.getoutput("python3 -m pip check").split("\n")
+        if not line and proc.poll() is not None:
+            break
+def main():
+    """main"""
+    update_package = [
+        o.split(" ")[0] for o in subprocess.getoutput("python3 -m pip list -o").split("\n")[2:]
     ]
-    if len(broken_package) == 1:
-        break
-    for packages in broken_package:
-        subprocess.getoutput(f"python3 -m pip install -U {packages}")
+
+    for packages in update_package:
+        get_lines(f"python3 -m pip install -U {packages}")
+
+    while True:
+        broken_package = [
+            o.split(" ")[0] for o in subprocess.getoutput("python3 -m pip check").split("\n")
+        ]
+        if len(broken_package) == 1:
+            break
+        for packages in broken_package:
+            get_lines(f"python3 -m pip install -U {packages}")
+
+
+if __name__ == '__main__':
+    main()
