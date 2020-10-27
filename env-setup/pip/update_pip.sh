087416cee00b7d64b889393b051b46361a4a6e9a
Fal318 <uznaytiuy0625@gmail.com>
2020-10-27 11:26:28 +0900

    :bug:bug fix

--- env-setup/pip/update_pip.sh
+++ env-setup/pip/update_pip.sh
@@ -1,10 +1,8 @@
 #! /bin/bash
-pip list -o | tail -n +3 | awk '{ print $1 }' | xargs pip install -U
-package=`pip check | awk '{ print $1 }'`
-if [ ${#package[0]} == "No" ]; then
-    echo "OK"
-else
+pip3 list -o | tail -n +3 | awk '{ print $1 }' | xargs pip3 install -U
+package=`pip3 check | awk '{ print $1 }'`
+if [ ! ${#package[0]} == "No" ]; then
     for p in $package; do
-        pip install -U $p
+        pip3 install -U $p
     done
 fi
