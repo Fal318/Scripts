fe929d6b72029ed5d53715db9036b3652a0043d3
Fal318 <uznaytiuy0625@gmail.com>
2020-11-06 15:54:55 +0900

    :recycle:Refactoring

--- env-setup/pip/update_pip.sh
+++ env-setup/pip/update_pip.sh
@@ -1,8 +1,3 @@
 #! /bin/bash
 python3 -m pip list -o | tail -n +3 | awk '{ print $1 }' | xargs python3 -m pip install -U
-package=`python3 -m pip check | awk '{ print $1 }'`
-if [ ! ${#package[0]} == "No" ]; then
-    for p in $package; do
-        python3 -m pip install -U $p
-    done
-fi
+python3 -m pip check | tail -n +1 | awk '{ print $1 }' | xargs -I{} sh -c 'if [ {} != "No" ]; then python3 -m pip install -U {}; else exit; fi'
